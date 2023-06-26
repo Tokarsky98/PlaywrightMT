be0f92acb16cf6e96678d6b3486aa9611e7eebac
Tokarsky98 <tokarski.mateusz5@gmail.com>
2023-06-26 21:43:47 +0200

    Missing login tests and creating inventory page

---
+++ pages/inventory.ts
@@ -0,0 +1,20 @@
+import { Locator, Page } from '@playwright/test';
+
+export class InventoryPage {
+    readonly page: Page;
+    readonly productList: Locator;
+    readonly burgerMenu: Locator;
+    readonly logoutLink: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.productList = page.locator('.inventory_container');
+        this.burgerMenu = page.getByRole('button', { name: 'Open Menu' })
+        this.logoutLink = page.locator('#logout_sidebar_link');
+    }
+
+    async logout() {
+        await this.burgerMenu.click();
+        await this.logoutLink.click();
+      }
+}
--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,8 +1,24 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from "../../pages/login";
+import { InventoryPage } from '../../pages/inventory';
 
 test('Login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.login(process.env.LOGIN!, process.env.PASSWORD!);
-    await expect(loginPage.productList).toBeVisible();
-  });
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  await loginPage.login(process.env.LOGIN!, process.env.PASSWORD!);
+  await expect(inventoryPage.productList).toBeVisible();
+});
+
+test('Logout', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  await loginPage.login(process.env.LOGIN!, process.env.PASSWORD!);
+  await inventoryPage.logout();
+  await expect(loginPage.loginLogo).toBeVisible();
+});
+
+test('Login fail', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.LOGIN!, 'wrongPassword');
+  await expect(loginPage.errorMessage).toContainText('Username and password do not match any user in this service');
+});
