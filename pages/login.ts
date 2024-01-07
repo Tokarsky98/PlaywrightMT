f7d12dacdcfe2b12a1935d063837942dea4de236
Tokarsky98 <31842153+Tokarsky98@users.noreply.github.com>
2024-01-07 15:42:29 +0100

    Checking out the repository and planning test scenarios (#5)

--- pages/login.ts
+++ pages/login.ts
@@ -12,7 +12,7 @@ export class LoginPage {
         this.page = page;
         this.loginInput = page.getByTestId('username');
         this.passwordInput = page.getByTestId('password');
-        this.loginButton = page.getByTestId('login-button'); //Would be better: page.getByRole('button', { name: 'login-button' });
+        this.loginButton = page.getByTestId('login-button'); // Would be better: page.getByRole('button', { name: 'login-button' });
         this.loginLogo = page.locator('.login_logo').filter({ hasText: 'Swag Labs' });
         this.errorMessage = page.getByTestId('error');
     }   
