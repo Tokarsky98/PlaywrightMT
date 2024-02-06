1c64f62875984d2f122fe6ee8188786bac1a5878
Mateusz <tokarski.mateusz5@gmail.com>
2024-02-06 19:18:03 +0100

    chore: update playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,36 +11,19 @@ dotevn.config({
 const config: PlaywrightTestConfig = {
     testDir: './tests',
     reporter: process.env.CI ? 'html' : 'list',
-    /* Run tests in files in parallel */
     fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    //forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    //retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    //workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    timeout: 2 * 60 * 1000,
-    expect: {
-        /**
-         * Maximum time expect() should wait for the condition to be met.
-         * For example in `await expect(locator).toHaveText();`
-         */
-        timeout: 15 * 1000,
-    },
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    retries: 1,
+    workers: 1,
+    timeout: 60 * 1000,
+    expect: { timeout: 15 * 1000 },
     use: {
-        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
         actionTimeout: 10 * 1000,
-        /* Maximum time each navigation such as `page.goto()` can take. */
-        navigationTimeout: 30 * 1000,
-        /* Set test id to use a custom data attribute for the tests. */
+        navigationTimeout: 15 * 1000,
         testIdAttribute: 'data-test',
-        /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'https://www.saucedemo.com/',
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'retain-on-failure',
+        video: 'retain-on-failure',
+        screenshot: 'only-on-failure',
     },
 
     /* Configure projects for major browsers */
@@ -49,44 +32,7 @@ const config: PlaywrightTestConfig = {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
-
-        // {
-        //  name: 'firefox',
-        //  use: { ...devices['Desktop Firefox'] },
-        // },
-
-        // {
-        //   name: 'webkit',
-        //   use: { ...devices['Desktop Safari'] },
-        // },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-        // },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
 };
 
 export default config;
