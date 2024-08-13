db545ce007dde040e4a20d0e8002f237ff960702
Mateusz <tokarski.mateusz5@gmail.com>
2024-08-13 20:10:56 +0200

    feat: reorganize login page

--- pages/login.ts
+++ pages/login.ts
@@ -1,28 +1,51 @@
 import { Locator, Page } from '@playwright/test';
 
-export class LoginPage {
-    readonly page: Page;
+class HeaderSection {
+    readonly loginLogo: Locator;
+
+    constructor(root: Locator) {
+        this.loginLogo = root.filter({ hasText: 'Swag Labs' });
+    }
+}
+
+class LoginSection {
     readonly loginInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
     readonly loginLogo: Locator;
     readonly errorMessage: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.loginInput = page.getByTestId('username');
-        this.passwordInput = page.getByTestId('password');
-        this.loginButton = page.getByTestId('login-button'); // Would be better: page.getByRole('button', { name: 'login-button' });
-        this.loginLogo = page
+    constructor(root: Locator) {
+        this.loginInput = root.getByTestId('username');
+        this.passwordInput = root.getByTestId('password');
+        this.loginButton = root.getByTestId('login-button');
+        this.loginLogo = root
             .locator('.login_logo')
             .filter({ hasText: 'Swag Labs' });
-        this.errorMessage = page.getByTestId('error');
+        this.errorMessage = root.getByTestId('error');
     }
 
     async login(login: string, password: string): Promise<void> {
-        await this.page.goto('/');
         await this.loginInput.fill(login);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
+    }
+}
+
+export class Login {
+    readonly page: Page;
+    readonly headerSection: HeaderSection;
+    readonly loginSection: LoginSection;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.headerSection = new HeaderSection(page.locator('.login_logo'));
+        this.loginSection = new LoginSection(
+            page.locator('div[class="login_wrapper-inner"]'),
+        );
+    }
+
+    async goto(): Promise<void> {
+        await this.page.goto('/');
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,27 +1,40 @@
 import { test } from '@playwright/test';
 import { expect } from '@playwright/test';
-import { LoginPage } from '../pages/login';
+import { Login } from '../pages/login';
 import { InventoryPage } from '../pages/inventory';
 
-test('Login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+test('Login to the shop', async ({ page }) => {
+    const login = new Login(page);
+    const headerSection = login.headerSection;
+    const loginSection = login.loginSection;
     const inventoryPage = new InventoryPage(page);
-    await loginPage.login(process.env.LOGIN!, process.env.PASSWORD!);
+
+    await login.goto();
+    await expect(headerSection.loginLogo).toBeVisible();
+
+    await loginSection.login(process.env.LOGIN!, process.env.PASSWORD!);
     await expect(inventoryPage.productList).toBeVisible();
 });
 
-test('Logout', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+test('Logout from the shop', async ({ page }) => {
+    const login = new Login(page);
+    const headerSection = login.headerSection;
+    const loginSection = login.loginSection;
     const inventoryPage = new InventoryPage(page);
-    await loginPage.login(process.env.LOGIN!, process.env.PASSWORD!);
+
+    await login.goto();
+    await loginSection.login(process.env.LOGIN!, process.env.PASSWORD!);
     await inventoryPage.logout();
-    await expect(loginPage.loginLogo).toBeVisible();
+    await expect(headerSection.loginLogo).toBeVisible();
 });
 
 test('Login fail', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.login(process.env.LOGIN!, 'wrongPassword');
-    await expect(loginPage.errorMessage).toContainText(
-        'Username and password do not match any user in this service',
-    );
+    const login = new Login(page);
+    const loginSection = login.loginSection;
+    const message =
+        'Username and password do not match any user in this service';
+
+    await login.goto();
+    await loginSection.login(process.env.LOGIN!, 'wrongPassword');
+    await expect(loginSection.errorMessage).toContainText(message);
 });
