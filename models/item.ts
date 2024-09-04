9b49d7cdf5a06bf54ed2038a650621ea27a16f26
Mateusz <tokarski.mateusz5@gmail.com>
2024-09-04 18:38:51 +0200

    docs: add jsdoc descriptions

--- models/item.ts
+++ models/item.ts
@@ -1,12 +1,28 @@
+/**
+ * Class with item data, created for fixture purpose.
+ *
+ * The fields in this class define which item will be added to the cart.
+ * There are default values for the class fields defined in the constructor.
+ * When no values provided, the default fields are used.
+ */
 export class Item {
     name: string;
     price: number;
 
+    /**
+     * Adds item to the cart by setting the class fields with provided values.
+     * @param name - The string value of the item name.
+     * @param price - The numeric value of the item price.
+     */
     constructor(name = 'Sauce Labs Backpack', price = 29.99) {
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Return adjusted price of item.
+     * @returns The string value with dollar sign and fixed-point notation.
+     */
     getDisplayFormat(): string {
         return `$${this.price.toFixed(2)}`;
     }
--- pages/cart.ts
+++ pages/cart.ts
@@ -2,20 +2,31 @@ import { Locator, Page } from '@playwright/test';
 import { CommonHeaderSection } from './commonHeader';
 import { Inventory } from './inventory';
 
+/** Secondary header section. */
 class SecondaryHeaderSection {
     readonly titleSpan: Locator;
 
+    /**
+     * Create the object.
+     * @param root - The locator of top-level element of section.
+     */
     constructor(root: Locator) {
         this.titleSpan = root.locator('.title');
     }
 }
 
+/** Cart list section. */
 class CartListSection {
     readonly page: Page;
     readonly cartList: Locator;
     readonly continueShoppingButton: Locator;
     readonly checkoutButton: Locator;
 
+    /**
+     * Create the object.
+     * @param page - The object representing a single tab in the browser.
+     * @param root - The locator of top-level element of section.
+     */
     constructor(page: Page, root: Locator) {
         this.page = page;
         this.cartList = root.locator('.cart_list');
@@ -25,28 +36,41 @@ class CartListSection {
         this.checkoutButton = root.getByRole('button', { name: 'Checkout' });
     }
 
+    /**
+     * Open Inventory view after clicking on the `Continue Shopping` button.
+     * @returns Inventory object representing the opened inventory page.
+     */
     async clickContinueShoppingButton(): Promise<Inventory> {
         await this.continueShoppingButton.click();
         return new Inventory(this.page);
     }
 
+    /**
+     * Get the names of all listed items in the cart.
+     * @returns Array of string values representing the names of items in the cart.
+     */
     async getNamesofItems(): Promise<string[]> {
         const names = await this.cartList.locator('.cart_item').all();
         const cartItemNames = await Promise.all(
             names.map(async (name) => {
-                return name.locator('.inventory_item_name').textContent();
+                return await name.locator('.inventory_item_name').textContent();
             }),
         );
         return cartItemNames.filter((name) => name !== null) as string[];
     }
 }
 
+/** Cart view. */
 export class Cart {
     readonly page: Page;
     readonly primaryHeaderSection: CommonHeaderSection;
     readonly secondaryHeaderSection: SecondaryHeaderSection;
     readonly cartListSection: CartListSection;
 
+    /**
+     * Create object.
+     * @param page - The object representing a single tab in the browser.
+     */
     constructor(page: Page) {
         this.page = page;
         this.primaryHeaderSection = new CommonHeaderSection(page);
@@ -59,6 +83,7 @@ export class Cart {
         );
     }
 
+    /** Visit Cart page. */
     async goto(): Promise<void> {
         await this.page.goto('/cart.html');
     }
