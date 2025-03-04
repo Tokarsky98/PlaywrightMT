fff82987a17f5853de603ecff506b277a4a14750
Mateusz <tokarski.mateusz5@gmail.com>
2025-03-04 15:48:56 +0100

    refactor: automated login fixture

--- fixtures/automatedLogin.ts
+++ fixtures/automatedLogin.ts
@@ -1,12 +1,11 @@
 import { Page } from 'playwright';
 import { Login } from '../pages/login';
+import { standardUser } from '../test-data/login.data';
 
 export const automatedLogin = async (page: Page): Promise<void> => {
     const login = new Login(page);
     const loginSection = login.loginSection;
 
     await page.goto('/');
-    await loginSection.loginInput.fill(process.env.LOGIN!);
-    await loginSection.passwordInput.fill(process.env.PASSWORD!);
-    await loginSection.loginButton.click();
+    await loginSection.login(standardUser);
 };
