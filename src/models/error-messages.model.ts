a0375397311687631952732bfa31fe9eb8cd65e6
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-06 13:42:09 +0100

    refactor: add error message for locked user

--- src/models/error-messages.model.ts
+++ src/models/error-messages.model.ts
@@ -2,4 +2,5 @@ export interface ErrorMessagesModel {
     invalidCredentials: string;
     emptyUsername: string;
     emptyPassword: string;
+    lockedUser: string;
 }
--- src/test-data/error-messages.data.ts
+++ src/test-data/error-messages.data.ts
@@ -2,7 +2,8 @@ import { ErrorMessagesModel } from '@_src/models/error-messages.model';
 
 export const errorMessages: ErrorMessagesModel = {
     invalidCredentials:
-        'Username and password do not match any user in this service',
+        'Epic sadface: Username and password do not match any user in this service',
     emptyUsername: 'Epic sadface: Username is required',
     emptyPassword: 'Epic sadface: Password is required',
+    lockedUser: 'Epic sadface: Sorry, this user has been locked out.',
 };
