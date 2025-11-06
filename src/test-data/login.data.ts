fdda1a3a3e50d7ce034ab51aa6aa258b99151c0c
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-06 13:48:53 +0100

    refactor: update data file for login with invalid user objects

--- src/test-data/login.data.ts
+++ src/test-data/login.data.ts
@@ -5,3 +5,27 @@ export const standardUser: LoginModel = {
     username: USERNAME,
     password: PASSWORD,
 };
+
+export const lockedUser: LoginModel = {
+    username: 'locked_out_user',
+    password: PASSWORD,
+};
+
+export const invalidUsers = {
+    wrongPassword: {
+        username: USERNAME,
+        password: 'wrongPassword',
+    },
+    emptyUsername: {
+        username: '',
+        password: PASSWORD,
+    },
+    emptyPassword: {
+        username: USERNAME,
+        password: '',
+    },
+    emptyBoth: {
+        username: '',
+        password: '',
+    },
+};
