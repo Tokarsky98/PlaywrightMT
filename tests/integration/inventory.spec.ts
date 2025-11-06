7ecc6309adc58e767c007d09e5407a1fb8704ad1
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-06 13:28:00 +0100

    refactor: improve test titles for inventory

--- tests/integration/inventory.spec.ts
+++ tests/integration/inventory.spec.ts
@@ -3,126 +3,138 @@ import { itemPairs } from '@_src/test-data/inventory.data';
 
 const { twoItems } = itemPairs;
 
-test('check addition and removal of items from the cart @logged', async ({
-    inventoryPage,
-}) => {
-    const { header, inventorySection } = inventoryPage;
+test.describe('Inventory - Cart Operations', () => {
+    test('should add and remove items from the cart @logged', async ({
+        inventoryPage,
+    }) => {
+        const { header, inventorySection } = inventoryPage;
 
-    await expect(inventorySection.productList).toBeVisible();
+        await expect(inventorySection.productList).toBeVisible();
 
-    // Add items to the cart and check if they are there
-    const addedItems = await inventorySection.actionOnCart('add', twoItems);
-    await expect(header.cartBadge).toBeVisible();
-    await expect(header.cartBadge).toHaveText(`${addedItems.quantity}`);
+        // Add items to the cart and check if they are there
+        const addedItems = await inventorySection.actionOnCart('add', twoItems);
+        await expect(header.cartBadge).toBeVisible();
+        await expect(header.cartBadge).toHaveText(`${addedItems.quantity}`);
 
-    const cartView = await header.clickCartIcon();
-    expect(await cartView.cartListSection.getNamesOfItems()).toEqual(twoItems);
+        const cartView = await header.clickCartIcon();
+        expect(await cartView.cartListSection.getNamesOfItems()).toEqual(
+            twoItems,
+        );
 
-    // Back to the `Inventory` view
-    const inventoryView =
-        await cartView.cartListSection.clickContinueShoppingButton();
+        // Back to the `Inventory` view
+        const inventoryView =
+            await cartView.cartListSection.clickContinueShoppingButton();
 
-    // Remove items from the cart and check if they are no longer there
-    await inventoryView.inventorySection.actionOnCart('remove', twoItems);
-    await expect(header.cartBadge).toBeHidden();
+        // Remove items from the cart and check if they are no longer there
+        await inventoryView.inventorySection.actionOnCart('remove', twoItems);
+        await expect(header.cartBadge).toBeHidden();
 
-    const cartViewAgain = await header.clickCartIcon();
-    expect(await cartViewAgain.cartListSection.getNamesOfItems()).toEqual([]);
+        const cartViewAgain = await header.clickCartIcon();
+        expect(await cartViewAgain.cartListSection.getNamesOfItems()).toEqual(
+            [],
+        );
+    });
+
+    test('should check the total price of added items @logged', async ({
+        inventoryPage,
+    }) => {
+        const inventorySection = inventoryPage.inventorySection;
+
+        await expect(inventorySection.productList).toBeVisible();
+
+        const addedItems = await inventorySection.actionOnCart('add', twoItems);
+
+        const totalPrice = await inventorySection.calculateTotalPriceWithoutTax(
+            addedItems.names,
+        );
+
+        const checkoutStepTwoView = await inventoryPage.goToCheckoutStepTwo();
+        await expect(
+            checkoutStepTwoView.checkoutStepTwoSection.itemTotal,
+        ).toHaveText(`Item total: $${totalPrice}`);
+    });
 });
 
-test('check the total price of added items @logged', async ({
-    inventoryPage,
-}) => {
-    const inventorySection = inventoryPage.inventorySection;
+test.describe('Inventory - Sorting Functionality', () => {
+    test('should check the sorting of items @logged', async ({
+        inventoryPage,
+    }) => {
+        const { inventorySection, secondaryHeader } = inventoryPage;
 
-    await expect(inventorySection.productList).toBeVisible();
+        const originalOrderOfNames =
+            await inventorySection.getItemsOrder('name');
+        const originalOrderOfPrices =
+            await inventorySection.getItemsOrder('price');
 
-    const addedItems = await inventorySection.actionOnCart('add', twoItems);
+        const sorting: {
+            filter: string;
+            orderBy: 'name' | 'price';
+            expectedOrder: string[][];
+        }[] = [
+            {
+                filter: 'Name (Z to A)',
+                orderBy: 'name',
+                expectedOrder: [
+                    originalOrderOfNames[5],
+                    originalOrderOfNames[4],
+                    originalOrderOfNames[3],
+                    originalOrderOfNames[2],
+                    originalOrderOfNames[1],
+                    originalOrderOfNames[0],
+                ],
+            },
+            {
+                filter: 'Name (A to Z)',
+                orderBy: 'name',
+                expectedOrder: [
+                    originalOrderOfNames[0],
+                    originalOrderOfNames[1],
+                    originalOrderOfNames[2],
+                    originalOrderOfNames[3],
+                    originalOrderOfNames[4],
+                    originalOrderOfNames[5],
+                ],
+            },
+            {
+                filter: 'Price (low to high)',
+                orderBy: 'price',
+                expectedOrder: [
+                    originalOrderOfPrices[4],
+                    originalOrderOfPrices[1],
+                    originalOrderOfPrices[2],
+                    originalOrderOfPrices[5],
+                    originalOrderOfPrices[0],
+                    originalOrderOfPrices[3],
+                ],
+            },
+            {
+                filter: 'Price (high to low)',
+                orderBy: 'price',
+                expectedOrder: [
+                    originalOrderOfPrices[3],
+                    originalOrderOfPrices[0],
+                    originalOrderOfPrices[5],
+                    originalOrderOfPrices[2],
+                    originalOrderOfPrices[1],
+                    originalOrderOfPrices[4],
+                ],
+            },
+        ];
 
-    const totalPrice = await inventorySection.calculateTotalPriceWithoutTax(
-        addedItems.names,
-    );
+        for (const { filter, orderBy, expectedOrder } of sorting) {
+            await secondaryHeader.filterSelect.selectOption({
+                label: filter,
+            });
 
-    const checkoutStepTwoView = await inventoryPage.goToCheckoutStepTwo();
-    await expect(
-        checkoutStepTwoView.checkoutStepTwoSection.itemTotal,
-    ).toHaveText(`Item total: $${totalPrice}`);
-});
+            const selectedFilterText =
+                await secondaryHeader.filterActiveOption.textContent();
+            expect(selectedFilterText).toContain(filter);
 
-test('check the sorting of items @logged', async ({ inventoryPage }) => {
-    const { inventorySection, secondaryHeader } = inventoryPage;
-
-    const originalOrderOfNames = await inventorySection.getItemsOrder('name');
-    const originalOrderOfPrices = await inventorySection.getItemsOrder('price');
-
-    const sorting: {
-        filter: string;
-        orderBy: 'name' | 'price';
-        expectedOrder: string[][];
-    }[] = [
-        {
-            filter: 'Name (Z to A)',
-            orderBy: 'name',
-            expectedOrder: [
-                originalOrderOfNames[5],
-                originalOrderOfNames[4],
-                originalOrderOfNames[3],
-                originalOrderOfNames[2],
-                originalOrderOfNames[1],
-                originalOrderOfNames[0],
-            ],
-        },
-        {
-            filter: 'Name (A to Z)',
-            orderBy: 'name',
-            expectedOrder: [
-                originalOrderOfNames[0],
-                originalOrderOfNames[1],
-                originalOrderOfNames[2],
-                originalOrderOfNames[3],
-                originalOrderOfNames[4],
-                originalOrderOfNames[5],
-            ],
-        },
-        {
-            filter: 'Price (low to high)',
-            orderBy: 'price',
-            expectedOrder: [
-                originalOrderOfPrices[4],
-                originalOrderOfPrices[1],
-                originalOrderOfPrices[2],
-                originalOrderOfPrices[5],
-                originalOrderOfPrices[0],
-                originalOrderOfPrices[3],
-            ],
-        },
-        {
-            filter: 'Price (high to low)',
-            orderBy: 'price',
-            expectedOrder: [
-                originalOrderOfPrices[3],
-                originalOrderOfPrices[0],
-                originalOrderOfPrices[5],
-                originalOrderOfPrices[2],
-                originalOrderOfPrices[1],
-                originalOrderOfPrices[4],
-            ],
-        },
-    ];
-
-    for (const { filter, orderBy, expectedOrder } of sorting) {
-        await secondaryHeader.filterSelect.selectOption({
-            label: filter,
-        });
-
-        const selectedFilterText =
-            await secondaryHeader.filterActiveOption.textContent();
-        expect(selectedFilterText).toContain(filter);
-
-        const presentOrder = await inventorySection.getItemsOrder(orderBy);
-        expect(
-            presentOrder,
-            `Should have the expected order when filtered by: ${filter}`,
-        ).toEqual(expectedOrder);
-    }
+            const presentOrder = await inventorySection.getItemsOrder(orderBy);
+            expect(
+                presentOrder,
+                `Should have the expected order when filtered by: ${filter}`,
+            ).toEqual(expectedOrder);
+        }
+    });
 });
